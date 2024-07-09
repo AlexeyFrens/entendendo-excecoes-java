e36093cc15d6f0a528ba7025e37c714c4e5dfca6
João Cláudio <leiloka46@gmail.com>
2024-07-09 20:42:14 -0300

    Very bad solution

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.example.model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room Number: ");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Check-in date (dd/mm/yyyy): ");
+        Date checkIn = sdf.parse(scanner.nextLine());
+        System.out.print("Check-out date (dd/mm/yyyy): ");
+        Date checkOut = sdf.parse(scanner.nextLine());
+
+        if(checkOut.before(checkIn)){
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }else{
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/mm/yyyy): ");
+            checkIn = sdf.parse(scanner.nextLine());
+            System.out.print("Check-out date (dd/mm/yyyy): ");
+            checkOut = sdf.parse(scanner.nextLine());
+
+            Date now = new Date();
+            if(checkIn.before(now) || checkOut.before(now)){
+                System.out.println("Error in reservation: Reservation dates for update must be future dates");
+            }else if(checkOut.before(checkIn)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            }else{
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
+        }
+    }
+}
